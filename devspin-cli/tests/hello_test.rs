c7ea1f3bac6c7634cbaa23985792854ca5da001c
m-e pasta <mepastacc@gmail.com>
2025-12-08 20:00:21 +0100

    added a small test to test thee CLI logic — may need more later

---
+++ devspin-cli/tests/hello_test.rs
@@ -0,0 +1,29 @@
+//! IF THIS TEST IS NOT MODIFIED AND IT FAILS: IT MEANS THAT THE CLI LOGIC HAS BEEN CHANGED TO A RLLY BAD LOGIC
+//! PLS ME, DO NOT CHANGE THIS LOGIC
+
+use assert_cmd::cargo::cargo_bin_cmd;
+use predicates::prelude::*;  // For .not()
+
+#[test]
+fn test_test_cmd_without_w_msg_flag() {
+    let mut cmd = cargo_bin_cmd!("devspin-cli");
+    cmd.arg("test-cmd");
+    cmd.assert()
+        .success()
+        .stdout(predicates::str::contains("DEVSPIN").not());
+}
+
+#[test]
+fn test_test_cmd_with_w_msg_flag() {
+    let mut cmd = cargo_bin_cmd!("devspin-cli");
+    cmd.arg("test-cmd").arg("--w-msg");
+    cmd.assert()
+        .success()
+        .stdout(predicates::str::contains("
+    ██████╗ ███████╗██╗   ██╗███████╗██████╗ ██╗███╗   ██╗
+    ██╔══██╗██╔════╝██║   ██║██╔════╝██╔══██╗██║████╗  ██║
+    ██║  ██║█████╗  ██║   ██║███████╗██████╔╝██║██╔██╗ ██║
+    ██║  ██║██╔══╝  ╚██╗ ██╔╝╚════██║██╔═══╝ ██║██║╚██╗██║
+    ██████╔╝███████╗ ╚████╔╝ ███████║██║     ██║██║ ╚████║
+    ╚═════╝ ╚══════╝  ╚═══╝  ╚══════╝╚═╝     ╚═╝╚═╝  ╚═══╝"));
+}

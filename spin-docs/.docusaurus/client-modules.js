7c2cc662efef1231673628ed739c7fcc707389f6
m-epasta <mepastaacc@outlook.com>
2025-12-17 18:31:17 +0000

    moved spin into his own folder

--- spin-docs/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/workspaces/ubuntu/devSpin/spin-docs/node_modules/infima/dist/css/default/default.css"),
-  require("/workspaces/ubuntu/devSpin/spin-docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/workspaces/ubuntu/devSpin/spin-docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/workspaces/ubuntu/devSpin/spin-docs/src/css/custom.css"),
-];
--- spin-docs/DEVHELP.md
+++
@@ -1,2 +0,0 @@
-npx docusaurus serve --port 3000
-// for launching app on port (dev)
